4187e68852bcb5523096e8f7a8cfd4f13907e006
Andrewkadz <Andrewkadziolka@gmail.com>
2025-08-09 01:39:57 -0600

    Add .1 bridge and converter script

---
+++ HRM_Python_Bridge/ConsoleModuleToPy.py
@@ -0,0 +1,8 @@
+# Generated from ConsoleModuleToPy.1
+// HΛRM FILE: Console.hrm
+CONSOLE_MODULE {
+    VERSION: 0.1
+    STATUS: ACTIVE
+    IDENTITY: HARMONIA_OS::CONSOLE
+    LOAD_MAP { roadmap → Roadmap.hrm }
+}
